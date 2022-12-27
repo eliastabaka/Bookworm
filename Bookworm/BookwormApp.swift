b6b34ac2eb8dbfa66e4b254a177e226c44aa5adc
Milosz Tabaka <miloszmtabaka@gmail.com>
2022-12-27 17:33:30 +0000

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Elias Tabaka on 27/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
