24c3cbd5501ecec807dd9b55dc5e158b6af8305e
Milosz Tabaka <miloszmtabaka@gmail.com>
2022-12-27 18:35:57 +0000

    How to combine Core Data and SwiftUI

---
+++ Bookworm/DataController.swift
@@ -0,0 +1,21 @@
+//
+//  DataController.swift
+//  Bookworm
+//
+//  Created by Elias Tabaka on 27/12/2022.
+//
+
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "Bookworm")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+}
