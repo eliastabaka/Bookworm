516b6f05d9a47dc27df3b9697023581779dae45c
Elias Tabaka <mmtabaka1@sheffield.ac.uk>
2023-06-11 22:03:38 +0100

    Showing book details

---
+++ Bookworm/DetailView.swift
@@ -0,0 +1,42 @@
+//
+//  DetailView.swift
+//  Bookworm
+//
+//  Created by Elias Tabaka on 11/06/2023.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let book: Book
+    
+    var body: some View {
+        ScrollView {
+            ZStack(alignment: .bottomTrailing) {
+                Image(book.genre ?? "Fantasy")
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(book.genre?.uppercased() ?? "FANTASY")
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(8)
+                    .foregroundColor(.white)
+                    .background(.black.opacity(0.75))
+                    .clipShape(Capsule())
+                    .offset(x: -5, y: -5)
+            }
+            Text(book.author ?? "Unknown Author")
+                .font(.title)
+                .foregroundColor(.secondary)
+            
+            Text(book.review ?? "No review")
+                .padding()
+            
+            RatingView(rating: .constant(Int(book.rating)))
+                .font(.largeTitle)
+        }
+        .navigationTitle(book.title ?? "Unknown book")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
