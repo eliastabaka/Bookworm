d2f3febc4bdb5bb54cd96ba9c865f737e83838c0
Elias Tabaka <mmtabaka1@sheffield.ac.uk>
2023-06-11 18:00:26 +0100

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Elias Tabaka on 11/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
